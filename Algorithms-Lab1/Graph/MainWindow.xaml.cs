7e8dec2c87c038ff75496857e771d71966e2b9f3
el.petryaeva <el.petryaeva@mail.ru>
2024-10-01 21:34:14 +0500

    верстка окна

--- Algorithms-Lab1/Graph/MainWindow.xaml.cs
+++ Algorithms-Lab1/Graph/MainWindow.xaml.cs
@@ -1,1 +1,51 @@
-﻿
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System.Windows;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Создаем график
+            cartesianChart.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Пример данных",
+                    Values = new ChartValues<double> { 3, 5, 7, 4, 6 }
+                }
+            };
+
+            // Настройка осей
+            cartesianChart.AxisX.Add(new Axis
+            {
+                Title = "X Axis",
+                Labels = new[] { "A", "B", "C", "D", "E" }
+            });
+
+            cartesianChart.AxisY.Add(new Axis
+            {
+                Title = "Y Axis",
+                LabelFormatter = value => value.ToString("N")
+            });
+            
+            cartesianChart.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Серия 1",
+                    Values = new ChartValues<double> { 3, 5, 7, 4, 6 }
+                },
+                new LineSeries
+                {
+                    Title = "Серия 2",
+                    Values = new ChartValues<double> { 2, 6, 5, 7, 3 }
+                }
+            };
+        }
+    }
+}
