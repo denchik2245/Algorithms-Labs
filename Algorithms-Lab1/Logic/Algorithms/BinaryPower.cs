af070a846c9c47e2b87b5de0f99a761cb29578f0
Max Razin <razinmax02@gmail.com>
2024-10-06 16:40:35 +0500

    BinaryPower fixed

---
+++ Algorithms-Lab1/Logic/Algorithms/BinaryPower.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic.Algorithms
+{
+    public class BinaryPower : IPowerRaiser
+    {
+        public void RaiseToPower(int[] array, int power)
+        {
+            if (power < 0) throw new ArgumentException("Степень должна быть неотрицательным числом");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = BinPower(array[i], power);
+            }
+        }
+
+        private static int BinPower(int num, int power)
+        {
+            int result = 1;
+            while (power > 0)
+            {
+                if ((power & 1) == 1)
+                { // Если power нечётное
+                    result *= num;
+                }
+                num *= num; // Возводим num в квадрат
+                power >>= 1; // Делим power на 2
+            }
+            return result;
+        }
+    }
+}
